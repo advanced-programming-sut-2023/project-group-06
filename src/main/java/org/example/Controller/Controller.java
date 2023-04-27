6e9151229b5f330423dc8f8686ee40b8afe3849a
AMBandariM <ambandarim@gmail.com>
2023-04-27 16:54:10 -0700

    A bug fixed in loadMap

--- src/main/java/org/example/Controller/Controller.java
+++ src/main/java/org/example/Controller/Controller.java
@@ -13,133 +13,139 @@ import java.util.regex.Pattern;
 public class Controller {
     public static User currentUser;
     public static MenuType currentMenu;
-    public static void run(Scanner scanner){
+
+    public static void run(Scanner scanner) {
         Data.loadData("src/main/java/org/example/Model/data.json");
         if (Data.isStayLoggedIn()) currentMenu = MenuType.MAIN_MENU;
         else currentMenu = MenuType.START_MENU;
         while (true) {
             currentMenu = currentMenu.menu.run(scanner);
-            if(currentMenu == null) break;
+            if (currentMenu == null) break;
         }
     }
 
     private static String theLastCaptcha;
-    public static String getCaptcha(){
+
+    public static String getCaptcha() {
         theLastCaptcha = generator();
         return captchaGenerator(theLastCaptcha);
     }
-    public static boolean isCaptchaCorrect(String captcha){
+
+    public static boolean isCaptchaCorrect(String captcha) {
         return captcha.equals(theLastCaptcha);
     }
-    private static String generator(){
+
+    private static String generator() {
         Random random = new Random(10);
         Random rand = new Random();
         int min = 4;
         int max = 8;
-        int length = rand.nextInt(max-min) + min;
+        int length = rand.nextInt(max - min) + min;
         String chars = "0123465789";
         StringBuilder captcha = new StringBuilder();
-        while(length-- > 0){
-            int index = (int)(Math.random()*10);
+        while (length-- > 0) {
+            int index = (int) (Math.random() * 10);
             captcha.append(chars.charAt(index));
         }
         return captcha.toString();
     }
-    private static String captchaGenerator(String input){
-        String[][] digits = { {"⡀⡀⣄⣤⣶⣿⣶⣤⡀⡀⡀⡀",
-                               "⡀⡀⣾⣿⠳⡀⠉⣿⣷⡀⡀⡀",
-                               "⡀⢀⣿⡇⡀⡀⠈⢺⣿⡀⡀⡀",
-                               "⡀⢸⣿⡀⣴⣿⣆⠘⣿⡏⡀⡀",
-                               "⡀⢸⣿⡄⠈⠛⠁⢸⣿⠇⡀⡀",
-                               "⡀⡀⣿⣧⡀⡀⡀⣼⣿⡀⡀⡀",
-                               "⡀⡀⠽⣿⣷⣶⣾⣿⠧⡀⡀⡀",
-                               "⡀⡀⡀⡀⠈⠉⠉⡀⡀⡀⡀⡀"},
-                               {"⡀⡀⡀⡀⣀⣤⣤⡀⡀⡀⡀⡀",
-                                "⡀⡀⡀⣿⠿⢻⣿⡀⡀⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⢸⣿⡀⡀⡀⡀⡀",
-                                "⡀⢀⠁⡀⡀⢸⣿⡀⡀⡀⡀⡀",
-                                "⡀⡀⡀⡀⠤⢸⣿⡀⡀⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⢸⣿⡀⡀⡀⡀⡀",
-                                "⡀⡀⢰⣤⣤⣼⣿⣤⣤⣴⡀⡀",
-                                "⡀⡀⠈⠉⠉⠉⠉⠉⠉⠉⡀⡀"},
-                               {"⡀⡀⡀⣠⣴⣶⣶⣤⡀⡀⡀⡀",
-                                "⡀⡀⢿⡿⠉⠁⠉⢿⣿⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⢸⣿⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⣾⣿⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⢀⣾⡿⠁⡀⡀⡀",
-                                "⡀⡀⡀⢀⣴⣿⠋⡀⡀⡀⡀⡀",
-                                "⡀⡀⣿⣿⣿⣶⣶⣶⣶⣶⡀⡀",
-                                "⡀⡀⠉⠉⠉⠉⠉⠉⠉⠉⡀⡀"},
-                               {"⡀⡀⡀⣀⣤⣤⣤⣤⡀⡀⡀⡀",
-                                "⡀⡀⢿⡿⠛⠉⠙⢿⣿⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⣸⣿⡀⡀⡀",
-                                "⡀⡀⡀⡀⣤⣴⣾⠛⡀⡀⡀⡀",
-                                "⡀⡀⡀⡀⠉⠉⠛⣿⣷⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⠈⣿⡇⡀⡀",
-                                "⡀⡀⣦⣤⣀⣀⣤⣿⣿⡀⡀⡀",
-                                "⡀⡀⠉⠛⠛⠛⠛⠉⡀⡀⡀⡀"},
-                               {"⡀⡀⡀⡀⡀⡀⡀⣀⣀⡀⡀⡀",
-                                "⡀⡀⡀⣿⣿⡀⡀⣿⣿⡀⡀⡀",
-                                "⡀⡀⢀⣿⡇⡀⡀⣿⣿⡀⡀⡀",
-                                "⡀⡀⣸⣿⡀⡀⡀⣿⣿⡀⡀⡀",
-                                "⡀⡀⣿⣿⡀⡀⡀⣿⣿⡀⡀⡀",
-                                "⡀⢀⣿⣿⣿⣿⣿⣿⣿⣾⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⣾⣿⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⠿⠿⡀⡀⡀"},
-                               {"⡀⡀⢠⣤⣤⣤⣤⣤⣴⡀⡀⡀",
-                                "⡀⡀⢸⣿⠍⠉⠉⠉⠉⡀⡀⡀",
-                                "⡀⡀⢸⣿⡀⡀⡀⡀⡀⡀⡀⡀",
-                                "⡀⡀⢸⣿⡿⠿⢿⣿⣶⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⠈⣿⡇⡀⡀",
-                                "⡀⡀⣤⡆⡀⡀⡀⡀⣿⡏⡀⡀",
-                                "⡀⡀⢿⣿⣦⣤⣤⣿⣿⡀⡀⡀",
-                                "⡀⡀⡀⠈⠙⠛⠛⠉⡀⡀⡀⡀"},
-                               {"⡀⡀⡀⡀⡀⡀⣀⣤⣤⡀⡀⡀",
-                                "⡀⡀⡀⣠⣾⣿⠿⠛⠛⡀⡀⡀",
-                                "⡀⡀⢠⣿⠏⡀⡀⡀⡀⡀⡀⡀",
-                                "⡀⡀⣿⣿⣠⣾⣿⣿⣦⡀⡀⡀",
-                                "⡀⡀⣿⣷⠁⡀⡀⠈⣿⣧⡀⡀",
-                                "⡀⡀⣿⣧⡀⡀⡀⡀⣿⣿⡀⡀",
-                                "⡀⡀⢻⣿⣦⣀⣠⣾⣿⠃⡀⡀",
-                                "⡀⡀⡀⠈⠛⠛⠛⠋⡀⡀⡀⡀"},
-                               {"⡀⢠⣤⣤⣤⣤⣤⣤⣤⣤⡀⡀",
-                                "⡀⢸⣿⠛⠛⠛⠛⢛⣿⡏⡀⡀",
-                                "⡀⢸⣿⡀⡀⡀⡀⣿⣿⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⢰⣿⠇⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⣿⡿⡀⡀⡀⡀",
-                                "⡀⡀⡀⡀⡀⣼⣿⠁⡀⡀⡀⡀",
-                                "⡀⡀⡀⡀⢀⣿⡟⡀⡀⡀⡀⡀",
-                                "⡀⡀⡀⡀⠚⠛⡀⡀⡀⡀⡀⡀"},
-                               {"⡀⡀⡀⣠⣴⣶⣶⣤⡀⡀⡀⡀",
-                                "⡀⡀⣾⣿⠋⠉⠉⢿⣿⡀⡀⡀",
-                                "⡀⡀⢿⣿⡀⡀⡀⢸⣿⠁⡀⡀",
-                                "⡀⡀⡀⠙⣻⣷⣿⡛⠁⡀⡀⡀",
-                                "⡀⡀⣼⣿⠉⡀⠉⠻⣿⡄⡀⡀",
-                                "⡀⡀⣿⡇⡀⡀⡀⡀⣿⣷⡀⡀",
-                                "⡀⡀⢿⣿⣦⣤⣤⣾⣿⠃⡀⡀",
-                                "⡀⡀⡀⠈⠙⠛⠛⠉⡀⡀⡀⡀"},
-                               {"⡀⡀⡀⢀⣤⣤⣤⣤⡀⡀⡀⡀",
-                                "⡀⡀⣰⣿⠟⠉⠙⠻⣿⡆⡀⡀",
-                                "⡀⡀⣿⣿⡀⡀⡀⡀⣿⣿⡀⡀",
-                                "⡀⡀⣿⣿⡀⡀⡀⣠⣿⣿⡀⡀",
-                                "⡀⡀⠈⠿⣿⣿⡿⠃⣿⡿⡀⡀",
-                                "⡀⡀⡀⡀⡀⡀⡀⣼⣿⠁⡀⡀",
-                                "⡀⡀⡀⣤⣤⣶⣿⠿⠁⡀⡀⡀",
-                                "⡀⡀⡀⠛⠋⠉⡀⡀⡀⡀⡀⡀"}};
+
+    private static String captchaGenerator(String input) {
+        String[][] digits = {{"⡀⡀⣄⣤⣶⣿⣶⣤⡀⡀⡀⡀",
+                "⡀⡀⣾⣿⠳⡀⠉⣿⣷⡀⡀⡀",
+                "⡀⢀⣿⡇⡀⡀⠈⢺⣿⡀⡀⡀",
+                "⡀⢸⣿⡀⣴⣿⣆⠘⣿⡏⡀⡀",
+                "⡀⢸⣿⡄⠈⠛⠁⢸⣿⠇⡀⡀",
+                "⡀⡀⣿⣧⡀⡀⡀⣼⣿⡀⡀⡀",
+                "⡀⡀⠽⣿⣷⣶⣾⣿⠧⡀⡀⡀",
+                "⡀⡀⡀⡀⠈⠉⠉⡀⡀⡀⡀⡀"},
+                {"⡀⡀⡀⡀⣀⣤⣤⡀⡀⡀⡀⡀",
+                        "⡀⡀⡀⣿⠿⢻⣿⡀⡀⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⢸⣿⡀⡀⡀⡀⡀",
+                        "⡀⢀⠁⡀⡀⢸⣿⡀⡀⡀⡀⡀",
+                        "⡀⡀⡀⡀⠤⢸⣿⡀⡀⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⢸⣿⡀⡀⡀⡀⡀",
+                        "⡀⡀⢰⣤⣤⣼⣿⣤⣤⣴⡀⡀",
+                        "⡀⡀⠈⠉⠉⠉⠉⠉⠉⠉⡀⡀"},
+                {"⡀⡀⡀⣠⣴⣶⣶⣤⡀⡀⡀⡀",
+                        "⡀⡀⢿⡿⠉⠁⠉⢿⣿⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⢸⣿⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⣾⣿⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⢀⣾⡿⠁⡀⡀⡀",
+                        "⡀⡀⡀⢀⣴⣿⠋⡀⡀⡀⡀⡀",
+                        "⡀⡀⣿⣿⣿⣶⣶⣶⣶⣶⡀⡀",
+                        "⡀⡀⠉⠉⠉⠉⠉⠉⠉⠉⡀⡀"},
+                {"⡀⡀⡀⣀⣤⣤⣤⣤⡀⡀⡀⡀",
+                        "⡀⡀⢿⡿⠛⠉⠙⢿⣿⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⣸⣿⡀⡀⡀",
+                        "⡀⡀⡀⡀⣤⣴⣾⠛⡀⡀⡀⡀",
+                        "⡀⡀⡀⡀⠉⠉⠛⣿⣷⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⠈⣿⡇⡀⡀",
+                        "⡀⡀⣦⣤⣀⣀⣤⣿⣿⡀⡀⡀",
+                        "⡀⡀⠉⠛⠛⠛⠛⠉⡀⡀⡀⡀"},
+                {"⡀⡀⡀⡀⡀⡀⡀⣀⣀⡀⡀⡀",
+                        "⡀⡀⡀⣿⣿⡀⡀⣿⣿⡀⡀⡀",
+                        "⡀⡀⢀⣿⡇⡀⡀⣿⣿⡀⡀⡀",
+                        "⡀⡀⣸⣿⡀⡀⡀⣿⣿⡀⡀⡀",
+                        "⡀⡀⣿⣿⡀⡀⡀⣿⣿⡀⡀⡀",
+                        "⡀⢀⣿⣿⣿⣿⣿⣿⣿⣾⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⣾⣿⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⠿⠿⡀⡀⡀"},
+                {"⡀⡀⢠⣤⣤⣤⣤⣤⣴⡀⡀⡀",
+                        "⡀⡀⢸⣿⠍⠉⠉⠉⠉⡀⡀⡀",
+                        "⡀⡀⢸⣿⡀⡀⡀⡀⡀⡀⡀⡀",
+                        "⡀⡀⢸⣿⡿⠿⢿⣿⣶⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⠈⣿⡇⡀⡀",
+                        "⡀⡀⣤⡆⡀⡀⡀⡀⣿⡏⡀⡀",
+                        "⡀⡀⢿⣿⣦⣤⣤⣿⣿⡀⡀⡀",
+                        "⡀⡀⡀⠈⠙⠛⠛⠉⡀⡀⡀⡀"},
+                {"⡀⡀⡀⡀⡀⡀⣀⣤⣤⡀⡀⡀",
+                        "⡀⡀⡀⣠⣾⣿⠿⠛⠛⡀⡀⡀",
+                        "⡀⡀⢠⣿⠏⡀⡀⡀⡀⡀⡀⡀",
+                        "⡀⡀⣿⣿⣠⣾⣿⣿⣦⡀⡀⡀",
+                        "⡀⡀⣿⣷⠁⡀⡀⠈⣿⣧⡀⡀",
+                        "⡀⡀⣿⣧⡀⡀⡀⡀⣿⣿⡀⡀",
+                        "⡀⡀⢻⣿⣦⣀⣠⣾⣿⠃⡀⡀",
+                        "⡀⡀⡀⠈⠛⠛⠛⠋⡀⡀⡀⡀"},
+                {"⡀⢠⣤⣤⣤⣤⣤⣤⣤⣤⡀⡀",
+                        "⡀⢸⣿⠛⠛⠛⠛⢛⣿⡏⡀⡀",
+                        "⡀⢸⣿⡀⡀⡀⡀⣿⣿⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⢰⣿⠇⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⣿⡿⡀⡀⡀⡀",
+                        "⡀⡀⡀⡀⡀⣼⣿⠁⡀⡀⡀⡀",
+                        "⡀⡀⡀⡀⢀⣿⡟⡀⡀⡀⡀⡀",
+                        "⡀⡀⡀⡀⠚⠛⡀⡀⡀⡀⡀⡀"},
+                {"⡀⡀⡀⣠⣴⣶⣶⣤⡀⡀⡀⡀",
+                        "⡀⡀⣾⣿⠋⠉⠉⢿⣿⡀⡀⡀",
+                        "⡀⡀⢿⣿⡀⡀⡀⢸⣿⠁⡀⡀",
+                        "⡀⡀⡀⠙⣻⣷⣿⡛⠁⡀⡀⡀",
+                        "⡀⡀⣼⣿⠉⡀⠉⠻⣿⡄⡀⡀",
+                        "⡀⡀⣿⡇⡀⡀⡀⡀⣿⣷⡀⡀",
+                        "⡀⡀⢿⣿⣦⣤⣤⣾⣿⠃⡀⡀",
+                        "⡀⡀⡀⠈⠙⠛⠛⠉⡀⡀⡀⡀"},
+                {"⡀⡀⡀⢀⣤⣤⣤⣤⡀⡀⡀⡀",
+                        "⡀⡀⣰⣿⠟⠉⠙⠻⣿⡆⡀⡀",
+                        "⡀⡀⣿⣿⡀⡀⡀⡀⣿⣿⡀⡀",
+                        "⡀⡀⣿⣿⡀⡀⡀⣠⣿⣿⡀⡀",
+                        "⡀⡀⠈⠿⣿⣿⡿⠃⣿⡿⡀⡀",
+                        "⡀⡀⡀⡀⡀⡀⡀⣼⣿⠁⡀⡀",
+                        "⡀⡀⡀⣤⣤⣶⣿⠿⠁⡀⡀⡀",
+                        "⡀⡀⡀⠛⠋⠉⡀⡀⡀⡀⡀⡀"}};
         StringBuilder output = new StringBuilder();
-        for(int i = 0; i < 8; i++){
-            for(Character c : input.toCharArray()) output.append(digits[c.charValue() - '0'][i]);
-            if(i < 7) output.append("\n");
+        for (int i = 0; i < 8; i++) {
+            for (Character c : input.toCharArray()) output.append(digits[c.charValue() - '0'][i]);
+            if (i < 7) output.append("\n");
         }
         makeNoise(output);
         return output.toString();
     }
-    private static void makeNoise(StringBuilder str){
-        for(int i = 0; i < str.length(); i++){
+
+    private static void makeNoise(StringBuilder str) {
+        for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '\n') continue;
             int r = 0;
-            for(int j = 0; j < 8; j++){
-                if(Math.random() < 0.1) r |= 1 << j;
+            for (int j = 0; j < 8; j++) {
+                if (Math.random() < 0.1) r |= 1 << j;
             }
             str.setCharAt(i, (char) (((str.charAt(i) - 10240) ^ r) + 10240));
         }
@@ -148,7 +154,7 @@ public class Controller {
     public static String makeEntryValid(String entry) {//it was protected before
         if (entry == null) return null;
         if (entry.isEmpty()) return entry;
-        if (entry.charAt(0)=='"') return entry.substring(1, entry.length() - 1);
+        if (entry.charAt(0) == '"') return entry.substring(1, entry.length() - 1);
         else return entry;
     }
 
