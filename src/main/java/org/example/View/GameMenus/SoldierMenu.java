9d0d08d7d756deba2cec3a60ec958e0d7e602a4d
mobin2357 <mobin.razavi2357@gmail.com>
2023-05-01 13:00:34 +0430

    patrol unit completed

--- src/main/java/org/example/View/GameMenus/SoldierMenu.java
+++ src/main/java/org/example/View/GameMenus/SoldierMenu.java
@@ -22,6 +22,8 @@ public class SoldierMenu extends Menu {
                 System.out.println(SoldierController.moveUnitWithType(matcher).message);
             else if((matcher = Commands.getMatcher(command, Commands.MOVE_UNITES_WITHOUT_TYPE)).find())
                 System.out.println(SoldierController.moveUnitWithoutType(matcher).message);
+            else if((matcher = Commands.getMatcher(command, Commands.PATROL)).find())
+                System.out.println(SoldierController.patrolUnit(matcher).message);
             else System.out.println(Response.INVALID_COMMAND.message);
         }
         return null;
