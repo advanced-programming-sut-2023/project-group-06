87ab3343b47617064f900afc683f9096c7fe8197
sinaazizeddin <sina.azizeddin@gmail.com>
2023-05-01 22:44:52 +0430

    added trap!

--- src/main/java/org/example/Model/BuildingGroups/Trap.java
+++ src/main/java/org/example/Model/BuildingGroups/Trap.java
@@ -3,9 +3,17 @@ package org.example.Model.BuildingGroups;
 import org.example.Model.Kingdom;
 
 public class Trap extends Building{
-    int damage;
-
+    private int damage;
+    private boolean canBeSeenByEnemy;
     public Trap(Kingdom owner, BuildingType buildingType, int xCoordinate, int yCoordinate) {
         super(owner, buildingType, xCoordinate, yCoordinate);
+    }
+
+    public boolean canBeSeenByEnemy() {
+        return canBeSeenByEnemy;
+    }
+
+    public void setCanBeSeenByEnemy(boolean canBeSeenByEnemy) {
+        this.canBeSeenByEnemy = canBeSeenByEnemy;
     }
 }
