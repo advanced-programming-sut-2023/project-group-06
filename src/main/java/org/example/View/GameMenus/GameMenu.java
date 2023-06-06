993ed8826bd64862f9059d7e3d396ffaac881489
AMBandariM <ambandarim@gmail.com>
2023-06-06 14:30:09 -0700

    gameMenu is started (I really hope it be the last "start")

--- src/main/java/org/example/View/GameMenus/GameMenu.java
+++ src/main/java/org/example/View/GameMenus/GameMenu.java
@@ -1,24 +1,44 @@
 package org.example.View.GameMenus;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import org.example.Controller.GameControllers.GameController;
-import org.example.Controller.GameControllers.KingdomController;
 import org.example.Controller.GameControllers.MapController;
-import org.example.View.Commands;
-import org.example.View.Response;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.regex.Matcher;
+import org.example.Model.Data;
+import org.example.Model.Tile;
 
 public class GameMenu extends Application {
+    static Stage stage;
+    public StackPane mainStackPane;
+    public BorderPane mainBorderPane;
+    public Canvas mainCanvas;
+    public HBox bottomHBox;
+    public Pane mainPane;
+    public Pane canvasPane;
+    // map things
+    int mapPointerX = 300;
+    int mapPointerY = 1000;
+    //
+
     @Override
     public void start(Stage stage) throws Exception {
-        Pane root = new Pane();
-        stage.setTitle("Main Menu");
-        stage.setScene(new Scene(root, 600, 400));
+        GameMenu.stage = stage;
+        mainStackPane = FXMLLoader.load(GameMenu.class.getResource("/FXML/GameMenu.fxml"));
+        Scene scene = new Scene(mainStackPane);
+        stage.setScene(scene);
         stage.show();
+    }
+
+    public void initialize() {
+        Tile[][] map = Data.loadMap("test");
+        mainCanvas.setHeight(800);
+        mainCanvas.setWidth(1000);
+        MapController.mapGraphicProcessor(mainCanvas, map, mapPointerX, mapPointerY);
     }
 }
